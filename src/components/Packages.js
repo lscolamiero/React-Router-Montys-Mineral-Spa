01eeb539c95c7e642aed2fac91d08d9aba46ff51
Carlos Sosa <isek7or@gmail.com>
2024-01-17 13:21:37 -0800

    upload

---
+++ src/components/Packages.js
@@ -0,0 +1,18 @@
+export default function Packages({ packages }) {
+    const display = packages.map((spaPackage, i) => {
+        return <li key={i}>{spaPackage}</li>
+    })
+    return (
+        <div>
+            <div className="packages">
+                <div className="packagesHeader">
+                    <h1>Our Packages</h1>
+                    <ul>
+                        {display}
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
